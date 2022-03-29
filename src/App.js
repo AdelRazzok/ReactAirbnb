4c23d4b2f3e690496819327b56fb735b2b701f7a
= <LHP6.arazzok@gmail.com>
2022-03-29 10:56:58 +0200

    add data.js file and map on it to create Card elements

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import cardData from './data'
 
 export default function App() {
+	const cardList = cardData.map(data => {
+		return (
+			<Card 
+				imgName={data.imgName}
+				ratingAverage={data.ratingAverage}
+				ratingNumber={data.ratingNumber}
+				cardText={data.cardText}
+				cardPrice={data.cardPrice}
+			/>
+		)
+	})
+
 	return (
 		<div className="App">
 			<Navbar />
 			<Hero />
 			<div className="card-carousel">
-				<Card
-					imgName='katie-zaferes.png'
-					ratingAverage='5.0'
-					ratingNumber='6'
-					cardText='Life lessons with Katie Zaferes'
-					cardPrice='136'
-				/>
-
-				<Card
-					imgName='wedding-photo.png'
-					ratingAverage='5.0'
-					ratingNumber='30'
-					cardText='Learn wedding photography'
-					cardPrice='125'
-				/>
-
-				<Card
-					imgName='mountain-biking.png'
-					ratingAverage='4.8'
-					ratingNumber='2'
-					cardText='Group Mountain Biking'
-					cardPrice='50'
-				/>
+				{cardList}
 			</div>
 		</div>
 	);
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export default [
+	{
+		imgName: 'katie-zaferes.png',
+		ratingAverage: '5.0',
+		ratingNumber: 6,
+		cardText: 'Life lessons with Katie Zaferes',
+		cardPrice: 136
+	},
+	{
+		imgName: 'wedding-photo.png',
+		ratingAverage: '5.0',
+		ratingNumber: 30,
+		cardText: 'Learn wedding photography',
+		cardPrice: 125
+	},
+	{
+		imgName: 'mountain-biking.png',
+		ratingAverage: '4.8',
+		ratingNumber: 2,
+		cardText: 'Group Mountain Biking',
+		cardPrice: 50
+	}
+]
